840607d0f78137292a9e4135314b54e6c7f82cc9
babyabd-debug <baby2005abd@gmail.com>
2026-05-20 14:14:11 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,17 +5,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "32381936"))
+  API_HASH = os.environ.get("API_HASH", "84b62a739a2f582d3594416eb7e6607f")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "8686648515:AAFMke3sp70d-1IinyurlkTwl8oeWgzOJlM")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "figuresguru_bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1003947600023"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "8487637963"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://nayixo5975:nayixo5975@cluster0.i93leyr.mongodb.net/?appName=Cluster0")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1003982351683")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003714682909"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -36,11 +36,11 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [ABD](https://t.me/babyabd218)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
+[Donate Me](https://t.me/babyabd218)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
